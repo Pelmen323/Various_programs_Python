f74b3f37f0590473aa8eb5647797957da8334e99
Pelmen323 <nozovka9@mail.ru>
2021-09-21 12:00:26 +0300

    Added integer generator

--- 8. Random string generator/random_string_generator_module.py
+++ 8. Random string generator/random_string_generator_module.py
@@ -3,7 +3,7 @@ import string
 russian_alphabet_upper ='ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ'    
 russian_alphabet_lower ='йцукенгшщзхъфывапролджэячсмитьбюё' 
 
-def generate_a_string_full(length: int, eng_upper: bool, eng_lower: bool, rus_upper: bool, rus_lower: bool, digits: bool, special_sym: bool) -> str:
+def generate_string_full(length: int, eng_upper: bool, eng_lower: bool, rus_upper: bool, rus_lower: bool, digits: bool, special_sym: bool) -> str:
     if length < 5000000:
         result = ''.join(random.choice((string.ascii_uppercase if eng_upper == True else '') + \
                                             (string.ascii_lowercase if eng_lower == True else '') + \
@@ -22,7 +22,7 @@ def generate_a_string_full(length: int, eng_upper: bool, eng_lower: bool, rus_up
 
     return result
 
-def generate_a_string_short(length: int, eng_letters: bool, digits: bool, special_sym: bool) -> str:
+def generate_string_short(length: int, eng_letters: bool, digits: bool, special_sym: bool) -> str:
     if length < 5000000:
         result = ''.join(random.choice((string.ascii_letters if eng_letters == True else '') + \
                                             (string.digits if digits == True else '') + \
@@ -35,7 +35,16 @@ def generate_a_string_short(length: int, eng_letters: bool, digits: bool, specia
 
     return result
 
+def generate_integer(length: int) -> int:
+    if length < 5000000:
+        result = int(''.join(random.choice(string.digits) for i in range(length)))
+    else:
+        result = ''.join(random.choice(string.digits) for i in range(5000000))
+        result = int(result*round(length/5000000))               
+
+    return result
 
 if __name__ == '__main__':
-    print(generate_a_string_full(100, False, True, False, False, True, True))
-    print(generate_a_string_short(25, True, True, False))
+    print(generate_string_full(100, False, True, False, False, True, True))
+    print(generate_string_short(25, True, True, False))
+    print(generate_integer(150))
