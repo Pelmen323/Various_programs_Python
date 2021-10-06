c2f164222c15d72c66b4c0d8e354a6225a067367
Pelmen323 <nozovka9@mail.ru>
2021-10-06 22:19:45 +0300

    Updated count_words program - switched to Counter to increase performance, added message about time spent

--- 9. Count words/Count words.py
+++ 9. Count words/Count words.py
@@ -1,33 +1,36 @@
-from tkinter import Tk, filedialog
+from tkinter import filedialog, Tk
 import string
+from collections import Counter
+from timeit import default_timer as timer
+
 
 def count_words():
     window = Tk()
     try:                                                                                                            # Open file
         with open(filedialog.askopenfilename(filetypes=[("Text files", "*.txt")]), 'r', encoding='utf-8') as file:
             text_file = file.read()
-    except: 
+    except Exception:
         print('Unable to open file')
         return
-    window.destroy()                                                                                                # To hide the window that is triggered automatically
+
+    window.destroy()
+    start = timer()
     for i in text_file:                                                                                             # Remove all punctuation from string
         if i in string.punctuation:
             text_file = text_file.replace(i, '')
 
     text_file = text_file.replace('\n', '').lower()                                                                 # Remove newline char and make all words lower
     text_split = [i for i in text_file.split(' ') if i != '']                                                       # Create a clear list with words
+    words_dict = (sorted(dict(Counter(text_split)).items(), key=lambda x: -x[1]))
 
-    words_dict = dict()
-    for word in text_split:
-        words_dict.update({word:text_split.count(word)})                                                            # Count the words in file
-
-    sorted_dict = sorted(words_dict.items(), key=lambda x:-x[1])                                                    # Sort the results
     print("Total words: " + str(len(text_split)))
     print("Unique words: " + str(len(words_dict)))
-    for i in sorted_dict:
-        print(i[0]+":\t\t"+str(i[1]))
+    print("Count\tword")
+    for i in words_dict:
+        print(f"{i[1]}:\t{i[0]}")
+    stop = timer()
+    print(f"The run is finished in {stop-start:.4f} seconds")
 
-if __name__=='__main__':
+if __name__ == '__main__':
     count_words()
     input("Press any key to exit ")
-    
